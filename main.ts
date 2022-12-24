8ab909f2667a838a774ebbc4221ef6a1ee188587
felixtsu <humaeks@gmail.com>
2022-12-24 11:36:07 +0800

    1能推2能判定胜利3两层不能推

--- main.ts
+++ main.ts
@@ -1,6 +1,52 @@
+function pushIfPossible (direction: number) {
+    locationInDirection = grid.add(grid.getLocation(playerSprite), DIRECTION_VECTORS[direction][0], DIRECTION_VECTORS[direction][1])
+    pushedLocationInDirection = grid.add(grid.getLocation(playerSprite), DIRECTION_VECTORS[direction][0] * 2, DIRECTION_VECTORS[direction][1] * 2)
+    if (tiles.tileAtLocationEquals(locationInDirection, assets.tile`myTile0`) || tiles.tileAtLocationEquals(locationInDirection, sprites.dungeon.stairLadder)) {
+        if (!(tiles.tileAtLocationIsWall(pushedLocationInDirection))) {
+            if (tiles.tileAtLocationEquals(locationInDirection, assets.tile`myTile0`)) {
+                tiles.setTileAt(locationInDirection, assets.tile`myTile`)
+            } else {
+                tiles.setTileAt(locationInDirection, sprites.dungeon.floorDark2)
+            }
+            tiles.setWallAt(locationInDirection, false)
+            if (tiles.tileAtLocationEquals(pushedLocationInDirection, assets.tile`myTile`)) {
+                tiles.setTileAt(pushedLocationInDirection, assets.tile`myTile0`)
+            } else {
+                tiles.setTileAt(pushedLocationInDirection, sprites.dungeon.stairLadder)
+            }
+            tiles.setWallAt(pushedLocationInDirection, true)
+            return true
+        }
+    }
+    return false
+}
+function checkWin () {
+    if (tiles.getTilesByType(assets.tile`myTile`).length == 0) {
+        canMove = false
+        playerSprite.sayText("Yay~")
+        pause(1000)
+        game.over(true)
+    }
+}
+function onDirectionButtonDown (direction: number, spriteImage: Image) {
+    if (canMove) {
+        playerSprite.setImage(spriteImage)
+        if (checkWall(direction)) {
+            if (checkBox(direction)) {
+                if (pushIfPossible(direction)) {
+                    info.changeScoreBy(1)
+                    grid.move(playerSprite, DIRECTION_VECTORS[direction][0], DIRECTION_VECTORS[direction][1])
+                    checkWin()
+                }
+            }
+        } else {
+            info.changeScoreBy(1)
+            grid.move(playerSprite, DIRECTION_VECTORS[direction][0], DIRECTION_VECTORS[direction][1])
+        }
+    }
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    playerSprite.y += -16
-    playerSprite.setImage(img`
+    onDirectionButtonDown(0, img`
         . . . . . . f f f f . . . . . . 
         . . . . f f e e e e f f . . . . 
         . . . f e e e f f e e e f . . . 
@@ -20,8 +66,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         `)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    playerSprite.y += 16
-    playerSprite.setImage(img`
+    onDirectionButtonDown(2, img`
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
         . . . f f f 2 2 2 2 f f f . . . 
@@ -40,9 +85,27 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . f f . . f f . . . . . 
         `)
 })
+function initConstants () {
+    UP = [0, -1]
+    RIGHT = [1, 0]
+    DOWN = [0, 1]
+    LEFT = [-1, 0]
+    DIRECTION_VECTORS = [
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+    ]
+}
+// 0 - up
+// 1 - right
+// 2 - down 
+// 3 - left
+function checkBox (direction: number) {
+    return tiles.tileAtLocationEquals(grid.add(grid.getLocation(playerSprite), DIRECTION_VECTORS[direction][0], DIRECTION_VECTORS[direction][1]), assets.tile`myTile0`) || tiles.tileAtLocationEquals(grid.add(grid.getLocation(playerSprite), DIRECTION_VECTORS[direction][0], DIRECTION_VECTORS[direction][1]), sprites.dungeon.stairLadder)
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    playerSprite.x += 16
-    playerSprite.setImage(img`
+    onDirectionButtonDown(1, img`
         . . . . . . f f f f f f . . . . 
         . . . . f f e e e e f 2 f . . . 
         . . . f f e e e e f 2 2 2 f . . 
@@ -62,8 +125,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         `)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    playerSprite.x += -16
-    playerSprite.setImage(img`
+    onDirectionButtonDown(3, img`
         . . . . f f f f f f . . . . . . 
         . . . f 2 f e e e e f f . . . . 
         . . f 2 2 2 f e e e e f f . . . 
@@ -82,24 +144,42 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . f f f . . . . . . . 
         `)
 })
+// 0 - up
+// 1 - right
+// 2 - down 
+// 3 - left
+function checkWall (direction: number) {
+    return tiles.tileAtLocationIsWall(grid.add(grid.getLocation(playerSprite), DIRECTION_VECTORS[direction][0], DIRECTION_VECTORS[direction][1]))
+}
+let LEFT: number[] = []
+let DOWN: number[] = []
+let RIGHT: number[] = []
+let UP: number[] = []
+let pushedLocationInDirection: tiles.Location = null
+let DIRECTION_VECTORS: number[][] = []
+let locationInDirection: tiles.Location = null
+let canMove = false
 let playerSprite: Sprite = null
-tiles.setCurrentTilemap(tilemap`level1`)
+initConstants()
+tiles.setCurrentTilemap(tilemap`level2`)
 playerSprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . . f f f f f f . . . . 
+    . . . . f f e e e e f 2 f . . . 
+    . . . f f e e e e f 2 2 2 f . . 
+    . . . f e e e f f e e e e f . . 
+    . . . f f f f e e 2 2 2 2 e f . 
+    . . . f e 2 2 2 f f f f e 2 f . 
+    . . f f f f f f f e e e f f f . 
+    . . f f e 4 4 e b f 4 4 e e f . 
+    . . f e e 4 d 4 1 f d d e f . . 
+    . . . f e e e 4 d d d d f . . . 
+    . . . . f f e e 4 4 4 e f . . . 
+    . . . . . 4 d d e 2 2 2 f . . . 
+    . . . . . e d d e 2 2 2 f . . . 
+    . . . . . f e e f 4 5 5 f . . . 
+    . . . . . . f f f f f f . . . . 
+    . . . . . . . f f f . . . . . . 
     `, SpriteKind.Player)
-tiles.placeOnTile(playerSprite, tiles.getTileLocation(4, 3))
+grid.place(playerSprite, tiles.getTileLocation(4, 3))
+canMove = true
+info.setScore(0)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,41 +2,50 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "级别2":
-            case "级别2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16], TileScale.Sixteen);
-            case "level1":
-            case "级别1":return tiles.createTilemap(hex`0a000a00000000010c0200000000000000050d070000000000010c0a0d0b0c02000000050d0d0d0d0d070000000406090d0806030000000000050d070000000000000004060300000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . 2 2 2 . . . . 
-. . . 2 . 2 . . . . 
-. 2 2 2 . 2 2 2 . . 
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0a000a0000010808080808020000000509090b0909070000000509090a090907000000050b0a090a0b070000000509090a0909070000000509090b090907000000040606060606030000000000000000000000000000000000000000000000000000000000000000`, img`
+. 2 2 2 2 2 2 2 . . 
 . 2 . . . . . 2 . . 
-. 2 2 2 . 2 2 2 . . 
+. 2 . . 2 . . 2 . . 
+. 2 . 2 . 2 . 2 . . 
+. 2 . . 2 . . 2 . . 
+. 2 . . . . . 2 . . 
+. 2 2 2 2 2 2 2 . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorDark2,sprites.dungeon.stairLadder,myTiles.tile1], TileScale.Sixteen);
+            case "level1":
+            case "级别1":return tiles.createTilemap(hex`0a000a00000000010c0200000000000000050f070000000000010c0a0e0b0c02000000050f0e0d0e0f070000000406090e0806030000000000050f070000000000000004060300000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . 2 2 2 . . . . 
+. . . 2 . 2 . . . . 
+. 2 2 2 2 2 2 2 . . 
+. 2 . 2 . 2 . 2 . . 
+. 2 2 2 2 2 2 2 . . 
 . . . 2 . 2 . . . . 
 . . . 2 2 2 . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorDark2], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorDark2,sprites.dungeon.stairLadder,myTiles.tile1], TileScale.Sixteen);
+            case "level0":
+            case "级别2":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000002030303040000000000090a0b010500000000000807070706000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . 2 2 2 2 2 . . . 
+. . 2 . 2 . 2 . . . 
+. . 2 2 2 2 2 . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.stairLadder], TileScale.Sixteen);
         }
         return null;
     })
@@ -44,6 +53,10 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
         }
         return null;
     })
